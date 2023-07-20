bc9d20363c1b3dd2d2f033d04d47497a4b2d94c3
eusouodaniel <danielrodriguesdrs331@gmail.com>
2023-07-19 21:26:17 -0300

    new: aula 19-07

---
+++ 1-primeira-aula/vite-exemplo/src/Coder.tsx
@@ -0,0 +1,14 @@
+import './Coder.css'
+
+export default function Coder() {
+    return (
+      <>
+        <div>
+          <p className="coder">Coder</p>
+          <form action="" method="post">
+            <input type="button" value="Clique no coder" />
+          </form>
+        </div>
+      </>
+    )
+  }
--- 1-primeira-aula/vite-exemplo/src/main.tsx
+++ 1-primeira-aula/vite-exemplo/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { App, Home } from './App.tsx'
+import Coder from './Coder.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Home />
+    <Coder />
   </React.StrictMode>,
 )
